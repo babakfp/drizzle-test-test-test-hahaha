06ff32193022361f82616fb645b7be5a13d748a8
babakfp <babak.bxf@gmail.com>
2024-02-09 06:19:34 -0800

    .

--- src/lib/server/schema/users.ts
+++ src/lib/server/schema/users.ts
@@ -1,5 +1,4 @@
-import { sql } from "drizzle-orm"
-import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core"
+import { text, sqliteTable } from "drizzle-orm/sqlite-core"
 
 export const users = sqliteTable("users", {
     id: text("id"),
